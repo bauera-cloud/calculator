827d13a4cbaebd57efa7f851fb8273a1e5639810
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-05-11 16:42:15 -0400

    Add styles to calculator, page, and footer

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let number;
-let operator;
-let number2;
-
 function add(num1, num2) {
     return num1 + num2
 }
@@ -18,36 +14,31 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-function operate(num1, operator, num2) {
-    if(operator === '+') { return add(num1, num2)}
-    if(operator === '-') { return subtract(num1, num2)}
-    if(operator === '*') { return multiply(num1, num2)}
-    if(operator === '/') { return divide(num1,num2)}
+function operate(operator, num1, num2) {
+    if (operator === '+') { return add(num1, num2) }
+    if (operator === '-') { return subtract(num1, num2) }
+    if (operator === '*') { return multiply(num1, num2) }
+    if (operator === '/') { return divide(num1, num2) }
 }
-
-//I want the array to be in the same order
-//I want an array of 3 elements for each operation
-//I want the numbers to be converted to typeof 'number'
-
 
 // converts '12+1' to [12, '+', 1]
 function convertExpressToArr(str) {
     let operator = str.split('').find((value) => !parseInt(value) && value !== '.')
     let num1 = Number(str.slice(0, str.indexOf(operator)))
     let num2 = Number(str.slice(str.indexOf(operator) + 1))
-    return [num1, operator, num2]
+    return [operator, num1, num2]
 }
 
+/* Plan
+    equals sign shouldn't be added to display
+*/
 
-//subproblem: more than 2 numbers - '12+12+12'
-//the operate fn will always have 3 values
-
-//subproblem: identifying operators - '12-12+12'
-
-//subproblem: using previous result after '=' to add to
-//the next set of numbers
-
-
+//subproblems:
+//  pressing '=' before entering all of the numbers causes a problem.
+//  display error message if user tries to divide by 0
+//  disable the decimal button if there's already one in the display or get 12.2.3.1
+//  change the calculator design with CSS
+//  add a backspace button to undo a number
 
 
 
@@ -59,36 +50,21 @@ let result;
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         let btn = button.classList.value;
-        if (btn === 'clear') { return display.value = '';} 
+        if (btn === 'clear') { return display.value = ''; }
         else if ((btn === 'equals') && display.value.length >= 3) {
-            result = String(operate(...convertExpressToArr(display.value)));
+            result = String(operate(...convertExpressToArr(display.value)).toFixed(2));
             display.value = result
-        }else {
+        } else {
             display.value += e.target.textContent
         }
     })
 })
 
 
-/* Plan
-    get operations working:
-        operations without equals should have a separate
-            result display below the expression display
-        '=' should clear display. Then add result to display
 
-    equals sign shouldn't be added to display
-    make the clear function working
-*/ 
+//Subproblems I don't have to fix...
 
-//Understand the problem:
-
-//Pseudocode:
-
-//receive input from button
-//change display to include numbers and operator
-//when '=' button is selected, clear display
-//change number values in expression from display to numbers
-//run operate function to get result
-//change result to a string
-//change display to equal result
-
+//subproblem: more than 2 numbers - '12+12+12'
+//subproblem: identifying operators - '12-12+12'
+//subproblem: using previous result after '=' to add to
+//the next set of numbers and use on other display
