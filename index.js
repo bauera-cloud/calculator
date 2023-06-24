ef9ff9f504867b4454e0c4497d9a94f1170d7bd5
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-06-24 12:32:15 -0400

    Altered the approach for index.js
    First, index.js used a string from the display.value in order to get
    the 1st and 2nd number plus the operator. Then it calculated the
    result of that. This approach was limited but the number of numbers
    and operators that could be used in the expression. Secondly,
    index.js had no way of restricting the user from making an invalid
    expression.
    
    Currently, the user cannot have the operator at an incorrect
    position. The user cannot have multiple decimals for each number.
    The user can enter any amount of numbers for each number in
    the expression. Finally, now the user can enter any amount of
    numbers and operators in the expression.

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('button');
+let clearButton = document.getElementById('clear-btn');
+let deleteButton = document.getElementById('delete-btn');
+let lastValue;
+
+let matchOperator;
+let lastIndexOfOperator;
+let lastNumber;
+
+
+//make the delete button work without adding it to the display
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        lastValue = display.value[display.value.length - 1];
+
+        let buttonString = e.target.textContent;
+        let number = button.classList.contains('number');
+        let decimal = button.classList.contains('decimal');
+        let operator = button.classList.contains('operator');
+        let negative = button.classList.value === 'negative operator';
+
+        if (button === clearButton) { display.value = ''; }
+        // if (button === deleteButton) { deletePrevValue(display.value, prevValue) }
+        if ((lastValue === undefined || isOperator(lastValue)) && (number || decimal || negative)) {
+            addToDisplay(buttonString)
+        }
+        if ((isNumber(lastValue) && decimal && !isDecimalInNumber(getLastNumber())) || (isNumber(lastValue) && (number || operator))) {
+            addToDisplay(buttonString)
+        }
+        //problem2: ------
+        if (isDecimal(lastValue) && (number || operator)) {
+            addToDisplay(buttonString)
+        }
+        console.log(getLastNumber())
+    })
+})
+
+
+function getLastNumber() {
+    matchOperator = display.value.match(/[\*\+/-]/gi);
+    lastIndexOfOperator = matchOperator ? display.value.lastIndexOf(matchOperator[matchOperator.length - 1]) : 0;
+    lastNumber = lastIndexOfOperator === 0 ? display.value.slice(0) : display.value.slice(lastIndexOfOperator + 1);
+    return lastNumber
+}
+
+function isDecimalInNumber(lastNumber) {
+    return /\./g.test(lastNumber)
+}
+
+function deletePrevValue(display, prevValue) {
+    if (display) {
+        return display.replace(prevValue, '')
+    } else {
+        return display
+    }
+}
+
+function isOperator(lastValue) {
+    return new RegExp('[\*\+/-]').test(lastValue)
+}
+
+function isNumber(lastValue) {
+    return /[\d]/.test(lastValue)
+}
+
+function isDecimal(lastValue) {
+    return /\./.test(lastValue);
+}
+
+function isNegative(lastValue) {
+    return /\-/.test(lastValue)
+}
+
+function addToDisplay(buttonString) {
+    return display.value += buttonString;
+}
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -13,101 +92,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
-
-function operate(operator, num1, num2) {
-    if (operator === '+') { return add(num1, num2) }
-    if (operator === '-') { return subtract(num1, num2) }
-    if (operator === '*') { return multiply(num1, num2) }
-    if (operator === '/') { return divide(num1, num2) }
-}
-
-// converts '12+1' to [12, '+', 1]
-function convertExpressToArr(str) {
-    let operator = str.split('').find((value) => !parseInt(value) && value !== '.')
-    let num1 = Number(str.slice(0, str.indexOf(operator)))
-    let num2 = Number(str.slice(str.indexOf(operator) + 1))
-    return [operator, num1, num2]
-}
-
-let display = document.querySelector('.display');
-let buttons = document.querySelectorAll('button');
-let numbers = Array.from(buttons).filter((button) => button.textContent)
-console.log(numbers)
-
-
-let result;
-
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        let btn = button.classList.value;
-        preventDisplayClearDeleteEquals(btn, e)
-        if (isOperator && !isNegativeBtn)
-            if (isClearBtn(btn)) display.value = '';
-        if (isDeleteBtn(btn)) { display.value = deletePrevValue(display.value) }
-        //                   && isValidExpression(string)
-        if (isEqualsBtn(btn) && display.value.length >= 3) {
-            result = String(operate(...convertExpressToArr(display.value)).toFixed(1));
-            display.value = result
-        }
-    })
-})
-
-function isClearBtn(btn) {
-    return btn === 'clear-btn'
-}
-
-function isEqualsBtn(btn) {
-    return btn === 'equals-btn'
-}
-
-function isDeleteBtn(btn) {
-    return btn === 'delete-btn'
-}
-
-function isNegativeBtn(btn) {
-    return btn === 'negative-btn'
-}
-
-function deletePrevValue(display) {
-    if (display) {
-        let prevValue = display[display.length - 1]
-        return display.replace(prevValue, '')
-    } else {
-        return display
-    }
-}
-
-function preventDisplayClearDeleteEquals(btn, e) {
-    if (!isClearBtn(btn) && !isDeleteBtn(btn) && !isEqualsBtn(btn)) {
-        display.value += e.target.textContent
-    }
-}
-
-function isOperator(string) {
-    let isOperatorRegex = new RegExp('[\*\+/-]')
-    let operator = isOperatorRegex.test(string)
-    return operator
-}
-console.log(isOperator('+'))
-
-let expression = '1+2'
-function isValidExpression(expression) {
-    console.log(expression.indexOf())
-    let expressionArr = expression.split('')
-    console.log(expressionArr)
-    return
-}
-console.log(isValidExpression(expression))
-
-
-
-
-
-
-
-//tests
-
-
 
 
 
